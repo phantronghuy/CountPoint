707727cd9f2aa2ae20fe9f08c0a01d5f6832ac42
phantronghuy <82666290+phantronghuy@users.noreply.github.com>
2022-08-25 00:02:38 +0700

    hehe

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_counter/UI/FoodPage.dart';
 import 'package:flutter_counter/UI/InputInformation.dart';
 import 'package:flutter_counter/objects/Player.dart';
 
@@ -15,8 +14,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final List<Player> players=[];
-
+  final List<Player> players = [];
+  final ValueNotifier<Player> _selectPlayerChange = ValueNotifier<Player>(new Player.Default());
   // This widget is the root of your application.
   static int _count = 0;
   /*
@@ -40,70 +39,89 @@ class _MyAppState extends State<MyApp> {
       padding: EdgeInsets.all(10),
     );
   }
-  _getContainter(){
-    return  Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            //color: Colors.green[200],
-            border: Border(
-              bottom: BorderSide(
-                color: Colors.black,
-                width: 1,
-              ),
-              top: BorderSide(
-                color: Colors.black,
-                width: 1,
-              ),
-              left: BorderSide(
-                color: Colors.black,
-                width: 1,
-              ),
-              right: BorderSide(
-                color: Colors.black,
-                width: 1,
-              ),
-            ),
-            borderRadius: BorderRadius.circular(20),
-          ),
-        ); 
+
+  void _awaitReturnValueFromSecondScreen(BuildContext context) async {
+    // start the SecondScreen and wait for it to finish with a result
+    final Player result = await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => InputInformation(),
+        ));
+    // after the SecondScreen result comes back update the Text widget with it
+    setState(() {
+      players[result.getId()].setName(result.getName());
+      print(result.getName());
+    });
   }
-  _getStackObject(int viTri) {
-    return Builder(
-      builder: (context)=>Column(
-        children: [
-          InkWell(
-      onTap:() {
-        print('Click');
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => InputInformation(),
-          )
-        );
-      },
-      child: Stack(
-      children: [
-        _getPadding(),
-        ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          clipBehavior: Clip.hardEdge,
-          child: FadeInImage.assetNetwork(
-            width: 100,
-            height: 100,
-            placeholder: 'assets/images/loading.gif',
-            image:
-                'https://scontent.fdad2-1.fna.fbcdn.net/v/t1.15752-9/296107224_604929331259626_4868231954046299990_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=ae9488&_nc_ohc=zB7dqQ_QL4UAX_88LxB&tn=MrJy_kwnCErs3i4z&_nc_ht=scontent.fdad2-1.fna&oh=03_AVIV3Or8ZD1VWzsKPvsmZB_hCP1zsgbxRSktZXNSUc68qA&oe=63246CD3',
+
+  _getContainter() {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        //color: Colors.green[200],
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.black,
+            width: 1,
+          ),
+          top: BorderSide(
+            color: Colors.black,
+            width: 1,
+          ),
+          left: BorderSide(
+            color: Colors.black,
+            width: 1,
+          ),
+          right: BorderSide(
+            color: Colors.black,
+            width: 1,
           ),
         ),
-      ],
-    ),
-    ),
-    Text('Player ${viTri}'),
-        ],
-      )
+        borderRadius: BorderRadius.circular(20),
+      ),
     );
   }
 
+  _getStackObject(int viTri) {
+    return Builder(
+        builder: (context) => ValueListenableBuilder(
+          valueListenable: _selectPlayerChange ,
+          builder:(context,pl,c){
+            return  Column(
+              children: [
+                InkWell(
+                  onTap: () {
+                    //
+                    print('Click');
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => InputInformation(),
+                    ));
+                     _awaitReturnValueFromSecondScreen(context);
+                  },
+                  child: Stack(
+                    children: [
+                      _getPadding(),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        clipBehavior: Clip.hardEdge,
+                        child: FadeInImage.assetNetwork(
+                          width: 100,
+                          height: 100,
+                          placeholder: 'assets/images/loading.gif',
+                          image:
+                              'https://scontent.fdad2-1.fna.fbcdn.net/v/t1.15752-9/296107224_604929331259626_4868231954046299990_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=ae9488&_nc_ohc=zB7dqQ_QL4UAX_88LxB&tn=MrJy_kwnCErs3i4z&_nc_ht=scontent.fdad2-1.fna&oh=03_AVIV3Or8ZD1VWzsKPvsmZB_hCP1zsgbxRSktZXNSUc68qA&oe=63246CD3',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Text('Player ${viTri + 1}'),
+              ],
+            ); 
+          }
+          ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -113,7 +131,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Counter App'),
         ),
-        body:Center(
+        body: Center(
           child: Column(
             children: <Widget>[
               _getPadding(),
@@ -133,8 +151,7 @@ class _MyAppState extends State<MyApp> {
                     onPressed: () {
                       setState(() {
                         print('giam');
-                        if(_count>0)
-                        players.removeLast();
+                        if (_count > 0) players.removeLast();
                         minusCountOne();
                         _getContainter();
                       });
@@ -157,11 +174,14 @@ class _MyAppState extends State<MyApp> {
                     padding: EdgeInsets.only(right: 10, left: 10),
                   ),
                   FloatingActionButton.extended(
+                    heroTag: "btn1",
                     onPressed: () {
+               
                       setState(() {
                         addCountOne();
                         print('tang');
-                        Player pl=new Player(_count,'Player ${_count}');
+                        Player pl = new Player(_count, 'Player ${_count}');
+                        print('${pl.getName()}');
                         players.add(pl);
                       });
                     },
@@ -185,17 +205,19 @@ class _MyAppState extends State<MyApp> {
                    if (_count > 0) _getStackObject(i), 
                   _getPadding(),
                 ],), */
-                   Row(children: [
-                     ListView.builder(
-                  
-                  //shrinkWrap: true,
+              SizedBox(
+                height: 300,
+                width: 300,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
                   itemCount: _count,
-                  itemBuilder: (context,viTri){
+                  itemBuilder: (context, viTri) {
                     return _getStackObject(viTri);
-                  },)
-                   ],)
-                  ] ,
-
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
